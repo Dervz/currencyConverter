1fe9620ae7826c12d7dc661f6206cf4d8c293e7f
dervz <nevmov@yahoo.co.uk>
2017-10-20 17:07:45 +0100

    Converter is functional

--- src/currencyconverter/CurrencyConverter.java
+++ src/currencyconverter/CurrencyConverter.java
@@ -10,26 +10,30 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Scanner;
 import com.google.gson.Gson;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class CurrencyConverter {
 
-    
+    private static final String API_HOST = "https://api.fixer.io";
 
     public static void main(String[] args) throws Exception {
 
-        String API_PROVIDER = "https://api.fixer.io";
-        String latest = CurrencyConverter.getJSON(API_PROVIDER + "/latest");
-        
+        String latest = CurrencyConverter.getJSON(API_HOST + "/latest");
+
+        //get latest rates,base & date to print them out in the console
         Gson gson = new Gson();
         JSONParser json = gson.fromJson(latest, JSONParser.class);
         System.out.println("date = " + json.getDate() + " , base = " + json.getBase());
 
+        //create HashMap to store rates after parsing
         HashMap<String, String> hm = new HashMap<>();
         hm = json.getRates();
         printMap(hm);
-   
-        //load GUI, passing the map to fullfil comboboxes
-        new GUI(hm).setVisible(true);
+
+        //load GUI, passing the map to fullfil comboboxes with its keys
+        GUI gui = new GUI(hm);
+        gui.setVisible(true);
 
     }
 
@@ -50,12 +54,36 @@ public class CurrencyConverter {
         return response;
     }
 
-    
-    public static void printMap(HashMap<String, String> map){
-     for (String name : map.keySet()) {
+    public static String convert(String currencyFrom, String currencyTo, Double amount) throws Exception {
+
+        if (currencyFrom != null && currencyTo != null) {
+            String response = getJSON(API_HOST + "/latest?base=" + currencyFrom);
+                
+            //get the rates from JSON format
+            Gson gson = new Gson();
+            JSONParser json = gson.fromJson(response, JSONParser.class);
+     
+            //rate of the currency against base
+            String rate = json.getRates().get(currencyTo);
+            double conversionRate= Double.valueOf((rate != null)?rate:"0.0");
+            
+            //calculate final answer and round up to 4 decimal places
+            DecimalFormat df = new DecimalFormat("#.####");
+            df.setRoundingMode(RoundingMode.CEILING);
+            double answer = amount * conversionRate;
+             
+            return amount + " " + currencyFrom + " = "  + df.format(answer) + " " + currencyTo  ;
+            
+        }
+
+        return "You did not select any currency";
+    }
+
+    public static void printMap(HashMap<String, String> map) {
+        for (String name : map.keySet()) {
             String value = map.get(name);
             System.out.println(name + " " + value);
         }
-    
+
     }
 }
--- src/currencyconverter/GUI.java
+++ src/currencyconverter/GUI.java
@@ -5,22 +5,30 @@
 package currencyconverter;
 
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JComboBox;
 
 public class GUI extends javax.swing.JFrame {
 
+    String firstCurrency;
+    String secondCurrency;
+    String amountValue;
+
     public GUI(HashMap<String, String> map) {
         setLookAndFeel();
         initComponents();
-         
-                
+
         for (String name : map.keySet()) {
-                currencyFrom.addItem(name);
-                currencyTo.addItem(name);
-            
+            currencyFrom.addItem(name);
+            currencyTo.addItem(name);
 
         }
-        
+
+    }
+
+    public void setAnswer(String ans) {
+        answer.setText(ans);
     }
 
     @SuppressWarnings("unchecked")
@@ -40,6 +48,12 @@ public class GUI extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jPanel1MouseClicked(evt);
+            }
+        });
+
         currencyFrom.setMaximumRowCount(200);
         currencyFrom.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -48,19 +62,35 @@ public class GUI extends javax.swing.JFrame {
         });
 
         currencyTo.setMaximumRowCount(200);
+        currencyTo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                currencyToActionPerformed(evt);
+            }
+        });
 
-        amount.setText("enter amount");
+        amount.setText("Enter amount");
+        amount.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                amountMouseClicked(evt);
+            }
+        });
         amount.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 amountActionPerformed(evt);
             }
         });
 
-        labelTo.setText("jLabel1");
+        labelTo.setText("to");
 
+        answer.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
         answer.setText("answer");
 
         convert.setText("Convert");
+        convert.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                convertActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -70,19 +100,18 @@ public class GUI extends javax.swing.JFrame {
                 .addGap(36, 36, 36)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(answer, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(convert)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(amount, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 81, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 84, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(currencyFrom, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(currencyTo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(labelTo))
-                        .addGap(114, 114, 114))))
+                        .addGap(114, 114, 114))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(convert)
+                            .addComponent(answer, javax.swing.GroupLayout.PREFERRED_SIZE, 265, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -97,9 +126,9 @@ public class GUI extends javax.swing.JFrame {
                 .addComponent(currencyTo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(17, 17, 17)
                 .addComponent(convert)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(18, 18, 18)
                 .addComponent(answer, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(58, Short.MAX_VALUE))
+                .addContainerGap(45, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -117,18 +146,67 @@ public class GUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void amountActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_amountActionPerformed
-        
+        setAmountValue(amount.getText());
     }//GEN-LAST:event_amountActionPerformed
 
     private void currencyFromActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_currencyFromActionPerformed
-        // TODO add your handling code here:
+        setFirstCurrency(currencyFrom.getSelectedItem().toString());
     }//GEN-LAST:event_currencyFromActionPerformed
 
+    private void currencyToActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_currencyToActionPerformed
+        setSecondCurrency(currencyTo.getSelectedItem().toString());
+    }//GEN-LAST:event_currencyToActionPerformed
 
-    
-   
+    private void convertActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_convertActionPerformed
+        CurrencyConverter converter = new CurrencyConverter();
+        String ans = "Calculation failed";
+        try {
+            
+            double amounts = Double.parseDouble(amount.getText());       
+            ans = converter.convert(getFirstCurrency(), getSecondCurrency(), amounts);
+        } catch (Exception ex) {
+           Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        answer.setText(ans);
+    }//GEN-LAST:event_convertActionPerformed
 
-    
+    private void amountMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_amountMouseClicked
+        if (amount.getText().equals("Enter amount")) {
+            amount.setText("");
+        }
+
+    }//GEN-LAST:event_amountMouseClicked
+
+    private void jPanel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MouseClicked
+        if (amount.getText().equals("")) {
+            amount.setText("Enter amount");
+        }
+    }//GEN-LAST:event_jPanel1MouseClicked
+
+    public void setAmountValue(String value) {
+        amountValue = value;
+    }
+
+    public String getAmountValue() {
+        return amountValue;
+    }
+
+    public void setFirstCurrency(String currency) {
+        firstCurrency = currency;
+    }
+
+    public void setSecondCurrency(String currency) {
+        secondCurrency = currency;
+    }
+
+    public String getFirstCurrency() {
+        return firstCurrency;
+    }
+
+    public String getSecondCurrency() {
+        return secondCurrency;
+    }
+
     public static void setLookAndFeel() {
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -158,6 +236,5 @@ public class GUI extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel labelTo;
     // End of variables declaration//GEN-END:variables
-
 
 }
